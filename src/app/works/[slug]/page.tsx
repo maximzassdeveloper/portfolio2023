36a58e66431d50c56615976a1eddcb8f84605030
maximzassdeveloper <fantom.zms14@gmail.com>
2024-02-21 00:02:34 +0300

    remove getStaticProps from single work

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -3,26 +3,32 @@ import { workService } from '@/shared/services/workService'
 import { notFound } from 'next/navigation'
 
 interface SingleWorkParams {
-	slug: string
+  slug: string
 }
 
-export async function generateStaticParams() {
-	const { data: works } = await workService.getWorks()
+// not build with next api
+// export async function generateStaticParams() {
+// 	const { data: works } = await workService.getWorks()
 
-	return works.map((work) => ({
-		slug: work.slug,
-	}))
-}
+// 	return works.map((work) => ({
+// 		slug: work.slug,
+// 	}))
+// }
 
 async function SingleWorkPage({ params }: { params: SingleWorkParams }) {
-	const response = await workService.getWork(params.slug)
-	const { work, nextWork } = response.data
+  const response = await workService.getWork(params.slug)
+  const { work, nextWork } = response.data
 
-	if (!work) {
-		notFound()
-	}
+  if (!work) {
+    notFound()
+  }
 
-	return <SingleWork work={work} nextWork={nextWork} />
+  return (
+    <SingleWork
+      work={work}
+      nextWork={nextWork}
+    />
+  )
 }
 
 export default SingleWorkPage
